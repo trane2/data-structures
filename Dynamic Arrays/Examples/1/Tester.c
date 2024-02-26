3acfdd90b069c95627fde170a1b8e5cb334c12bb
Eric Tran <trane2@oregonstate.edu>
2024-02-26 06:01:30 -0800

    Initialization

---
+++ Dynamic Arrays/Examples/1/Tester.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "dynarray.c"
+
+int main(int argc, char** argv) {
+    struct dynarray* dynarray = dynarray_create();
+
+    int val = 5;
+    void* yes = &val;
+    dynarray_insert(dynarray, yes);
+    
+    return 0;
+}
---
+++ LinkedLists/linkedlist.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+struct Student {
+    char* name;
+    int id;
+    float gpa;
+};
+
+int main(int argc, char** argv) {
+
+}
